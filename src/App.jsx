f6766dfcf843d521bc1255d2f042a91d435bfcaf
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2023-09-13 20:47:07 +0300

    WebDev Project TailWind Version 1

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import About from './Components/About';
+import Hero from './Components/Hero';
+import Navbar from './Components/Navbar';
+import Projects from './Components/Projects';
+import Skills from './Components/Skills';
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Skills />
+      <About />
+      <Projects />
+    </>
+  );
+};
+export default App;
---
+++ src/Components/SkillCard.jsx
@@ -0,0 +1,10 @@
+const SkillCard = ({ id, icon, text, title }) => {
+  return (
+    <article>
+      <span>{icon}</span>
+      <h4 className='mt-6 font-bold'>{title}</h4>
+      <p className='mt-2 text-slate-500'>{text}</p>
+    </article>
+  );
+};
+export default SkillCard;
